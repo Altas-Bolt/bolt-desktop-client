56d27e0d541b66e0045b79fcd155be24d05a0ea5
aniketbiswas21 <aniket.biswas75@gmail.com>
2022-08-26 00:50:21 +0530

    feat: hehe

--- src/renderer/windows/Dashboard/Minion/RegisterMinion/RegisterMinion.styles.tsx
+++ src/renderer/windows/Dashboard/Minion/RegisterMinion/RegisterMinion.styles.tsx
@@ -1,7 +1,45 @@
 import styled from 'styled-components';
 
 export const RegisterMinionWrapper = styled.div`
-  .formSection {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+
+  .card {
     display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #ffffff;
+    box-shadow: -6px 21px 70px -42px rgba(0, 0, 0, 0.36);
+
+    border-radius: 10px;
+    /* border: 1px solid grey; */
+    width: fit-content;
+    width: 450px;
+    /* height: 400px; */
+
+    .formSection {
+      display: flex;
+    }
+  }
+
+  .input {
+    input {
+      width: 100%;
+    }
+  }
+
+  .ant-row {
+    &.ant-form-item-row {
+      width: 400px;
+    }
+  }
+  .ant-input {
+    &.input {
+      width: 400px;
+    }
   }
 `;
--- src/renderer/windows/Dashboard/Minion/RegisterMinion/RegisterMinion.tsx
+++ src/renderer/windows/Dashboard/Minion/RegisterMinion/RegisterMinion.tsx
@@ -38,62 +38,67 @@ export const RegisterMinion: React.FC<RegisterMinionProps> = ({}) => {
 
   return (
     <RegisterMinionWrapper>
-      <div>
-        <PageHeader
-          className="site-page-header"
-          onBack={() => null}
-          title="Register a minion device"
-          backIcon={null}
-        />
-      </div>
+      <div className="card">
+        <div>
+          <PageHeader
+            className="site-page-header"
+            onBack={() => null}
+            title="Register a minion device"
+            backIcon={null}
+          />
+        </div>
 
-      <div className="formSection">
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          //   onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="ipv4"
-            name="ip"
-            rules={[
-              { required: true, message: 'Please input the minion ipv4!' },
-            ]}
+        <div className="formSection">
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            layout="vertical"
+            //   onFinishFailed={onFinishFailed}
+            autoComplete="off"
           >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="OS"
-            name="os"
-            rules={[{ required: true, message: 'Please input the minion OS!' }]}
-          >
-            <Select
-              style={{ width: 120 }}
-              // onChange={handleChange}
+            <Form.Item
+              label="ipv4"
+              name="ip"
+              style={{ width: '200px' }}
+              rules={[
+                { required: true, message: 'Please input the minion ipv4!' },
+              ]}
             >
-              {Object.values(OSEnum).map((os: string) => (
-                <Select.Option key={os}>{os}</Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
+              <Input className="input" />
+            </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Form.Item
+              label="OS"
+              name="os"
+              rules={[
+                { required: true, message: 'Please input the minion OS!' },
+              ]}
+            >
+              <Select
+                style={{ width: 400 }}
+                // onChange={handleChange}
+              >
+                {Object.values(OSEnum).map((os: string) => (
+                  <Select.Option key={os}>{os}</Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+
             <Button
               disabled={registerMinionMutation.isLoading}
               type="primary"
               htmlType="submit"
+              style={{ width: 400, marginBottom: '50px' }}
             >
               {registerMinionMutation.isLoading ? 'Loading' : 'Submit'}
             </Button>
-          </Form.Item>
-        </Form>
+          </Form>
+        </div>
+        <div>{minionId && <h2>Minion Id: {minionId}</h2>}</div>
       </div>
-      <div>{minionId && <h2>Minion Id: {minionId}</h2>}</div>
     </RegisterMinionWrapper>
   );
 };
